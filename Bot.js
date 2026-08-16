5cc039629d65971961d81748f38e1ebb89523fd1
neilp011 <araizvargas82@gmail.com>
2026-08-16 01:22:52 -0500

    Change URL from tu-red-social to Instagram

--- Bot.js
+++ Bot.js
@@ -5,7 +5,7 @@ async function main() {
   const browser = await chromium.launch();
   const page = await browser.newPage();
 
-  await page.goto('https://tu-red-social.com');
+  await page.goto('https://www.instagram.com/');
   console.log('Página cargada correctamente');
 
   await browser.close();
